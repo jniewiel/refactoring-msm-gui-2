ecc1f2612a885e61a367c6c460fc0f09204733e5
Jan Niewielski <39317379+jniewiel@users.noreply.github.com>
2024-06-20 18:48:37 +0000

    fixed actor errors

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -13,9 +13,8 @@
 class Actor < ApplicationRecord
   validates(:name, presence: true)
 
-  # has_many(:characters,
-  #          :class_name => "Character",
-  #          :foreign_key => "movie_id")
+  has_many(:characters,
+            :class_name => "Character")
 
   has_many(:movies,
            through: :characters,
@@ -25,13 +24,13 @@ class Actor < ApplicationRecord
            through: :characters,
            source: :movie)
 
-  def characters
-    key = self.id
+  # def characters
+  #   key = self.id
 
-    the_many = Character.where({ :actor_id => key })
+  #   the_many = Character.where({ :actor_id => key })
 
-    return the_many
-  end
+  #   return the_many
+  # end
 
   # def filmography
   #   the_many = Array.new
